363521af45876ce91daa8eb171932d2c2dbb4fbd
³£ÒæÃú <chang.yiming@trs.com.cn>
2018-07-07 19:22:32 +0800

    我的插件大致完成，仅仅在于自适应没办法 自由不同宽度，目前是统一宽度

--- src/pages/myplug/MyPlug.vue
+++ src/pages/myplug/MyPlug.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="basement">
     <div class="wrapper">
-      <div class="big-title">
+      <div class="big-title animation-fade-down">
         <p>一些我认为<strong>好用</strong>的插件推荐</p>
       </div>
       <!-- <div class="swiper-box">
@@ -14,44 +14,46 @@
           <div class="swiper-button-prev swiper-button-white"></div>
           <div class="swiper-button-next swiper-button-white"></div>
         </div> -->
-      <div class="swiper-box">
+      <div class="swiper-box animation-fade-down">
         <el-carousel indicator-position="outside">
           <el-carousel-item v-for="slide in swiperLists" :key="slide.id">
             <img class="spwier-img" :src="slide.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
       </div>
-      <div class="plug-box-wrap">
-        <el-row :gutter="20">
-          <el-col :xs="24" :sm="24" :md="16" :lg="16" :xl="16">
-            <div class="grid-content bg-hardgreen"></div>
+      <div class="plug-box-wrap animation-fade-up">
+        <el-row :gutter="20" v-for="(pluglist,index) in plugLists" :key="index">
+          <el-col v-for="plug in pluglist" :key="plug.id" :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
+            <div class="grid-content" :class="{'bg-hardgreen': plug.color === 'bg-hardgreen', 'bg-lightgreen': plug.color === 'bg-lightgreen'}">
+            <!-- class="grid-content" -->
+            <a :class="{'white-font': plug.color === 'bg-hardgreen', 'dark-font': plug.color === 'bg-lightgreen'}" target="_blank" :href="plug.link">
+            <p class="content-title">{{plug.title}}</p>
+            <p class="content-info" :title="plug.content">{{plug.content}}</p>
+            </a>
+            </div>
           </el-col>
-          <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
-            <div class="grid-content bg-normalgreen"></div>
-          </el-col>
+          <!-- <el-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8">
+            <div class="grid-content bg-normalgreen">
+              {{plug[1]}}
+            </div>
+          </el-col> -->
         </el-row>
-        <el-row :gutter="20">
+        <!-- <el-row :gutter="20">
           <el-col :xs="24" :sm="24" :md="14" :lg="14" :xl="14">
             <div class="grid-content bg-deepgreen"></div>
           </el-col>
-          <el-col :xs="24" :sm="24" :md="5" :lg="5" :xl="5">
+          <el-col :xs="24" :sm="24" :md="10" :lg="10" :xl="10">
             <div class="grid-content bg-lightgreen"></div>
-          </el-col>
-          <el-col :xs="24" :sm="24" :md="5" :lg="5" :xl="5">
-            <div class="grid-content bg-deepgreen"></div>
           </el-col>
         </el-row>
         <el-row :gutter="20">
-          <el-col :xs="24" :sm="24" :md="5" :lg="5" :xl="5">
+          <el-col :xs="24" :sm="24" :md="10" :lg="10" :xl="10">
             <div class="grid-content bg-lightgreen"></div>
           </el-col>
           <el-col :xs="24" :sm="24" :md="14" :lg="14" :xl="14">
             <div class="grid-content bg-hardgreen"></div>
           </el-col>
-          <el-col :xs="24" :sm="24" :md="5" :lg="5" :xl="5">
-            <div class="grid-content bg-normalgreen"></div>
-          </el-col>
-        </el-row>
+        </el-row> -->
       </div>
     </div>
   </div>
@@ -68,7 +70,8 @@ export default {
   },
   data() {
     return {
-      swiperLists: []
+      swiperLists: [],
+      plugLists: []
       // swiperOption: {
       //   speed: 800,
       //   autoplay: true,
@@ -87,26 +90,46 @@ export default {
     };
   },
   methods: {
+    groupBy(arr, len) {
+      let arr_len = arr.length;
+      let result = [];
+      for (let i = 0; i < arr_len; i += len) {
+        result.push(arr.slice(i, i + len));
+      }
+      return result;
+    },
     getSwiperListsData() {
-      this.$http({
-        url: "./api/myPlug.json",
-        methods: "GET"
-      }).then(
-        res => {
-          res = res.body;
-          if (res.success && res.data) {
-            const data = res.data;
-            this.swiperLists = data.swiperLists;
+      return new Promise((resolve, reject) => {
+        this.$http({
+          url: "./api/myPlug.json",
+          methods: "GET"
+        }).then(
+          res => {
+            res = res.body;
+            if (res.success && res.data) {
+              const data = res.data;
+              this.swiperLists = data.swiperLists;
+              this.plugLists = data.plugLists;
+              resolve();
+            }
+          },
+          err => {
+            reject(err);
           }
-        },
-        () => {
-          console.log("myPlug-error");
-        }
-      );
+        );
+      });
     }
   },
   mounted() {
-    this.getSwiperListsData();
+    this.getSwiperListsData().then(
+      () => {
+        this.plugLists = this.groupBy(this.plugLists, 2);
+        console.log(this.plugLists);
+      },
+      err => {
+        console.log("my-plug request data error", err);
+      }
+    );
   }
 };
 </script>
@@ -193,21 +216,41 @@ export default {
         background: #1F7252;
       }
 
-      .bg-normalgreen {
-        background: #208C60;
-      }
-
-      .bg-deepgreen {
-        background: #218868;
-      }
-
       .bg-lightgreen {
         background: #A0F4E2;
       }
 
       .grid-content {
         border-radius: 4px;
+        padding: 10px 20px;
         height: 152px;
+
+        .white-font {
+          color: #FFF;
+        }
+
+        .dark-font {
+          color: #333;
+        }
+
+        a:hover {
+          text-decoration: underline;
+        }
+
+        .content-title {
+          font-size: 20px;
+          margin: 15px 0;
+          font-weight: bolder;
+        }
+
+        .content-info {
+          line-height: 20px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 4;
+          -webkit-box-orient: vertical;
+        }
       }
 
       .row-bg {
@@ -215,6 +258,46 @@ export default {
         background-color: #f9fafc;
       }
     }
+
+    .animation-fade-down {
+      animation: fadeInDown 1000ms;
+      -webkit-animation: fadeInDown 1000ms;
+      -moz-animation: fadeInDown 1000ms;
+      -ms-animation: fadeInDown 1000ms;
+    }
+
+    .animation-fade-up {
+      animation: fadeInUp 1000ms;
+      -webkit-animation: fadeInUp 1000ms;
+      -moz-animation: fadeInUp 1000ms;
+      -ms-animation: fadeInUp 1000ms;
+    }
+  }
+}
+
+/* 淡入-从上down */
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+/* 淡入-从下up */
+@keyframes fadeInUp {
+  0% {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
   }
 }
 
@@ -233,6 +316,12 @@ export default {
 @media screen and (max-width: 332px) {
   .basement {
     top: 241px !important;
+  }
+}
+
+@media screen and (max-width: 991px) {
+  .el-col {
+    margin-top: 20px !important;
   }
 }
 </style>
